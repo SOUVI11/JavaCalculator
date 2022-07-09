9b65f92960fb54a7d02c30ed95e04f6e1b5bcffa
raravind7 <raravind600@gmail.com>
2022-07-10 01:31:00 +0530

    proper project file

---
+++ Calculator/src/Calculator/CalculatorDriver.java
@@ -0,0 +1,14 @@
+package Calculator;
+
+import javax.swing.JFrame;
+
+public class CalculatorDriver 
+{
+	public static void main(String[] args) 
+	{
+		Calculator myWindow = new Calculator();//create a new window
+		myWindow.setLocation(500, 200);// set the location of the window
+		myWindow.setVisible(true);// set the window to visible
+		myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// set the window to close when the X is clicked
+	}
+}
